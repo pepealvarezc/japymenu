d1f8bf0a6f22a4052abc4162ba3ec1aa0542e1f8
pepealvarezc <alvarez.m2308@gmail.com>
2025-04-10 20:20:04 -0600

    fix(mongo): mongo connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,24 +1,13 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI!;
+const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client: MongoClient;
-let clientPromise: Promise<MongoClient>;
-
-if (!uri) throw new Error("Falta MONGODB_URI en .env.local");
-
-if (process.env.NODE_ENV === "development") {
-  // Usamos la promesa global solo en desarrollo para evitar múltiples conexiones
-  if (!globalThis.mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    globalThis.mongoClientPromise = client.connect();
-  }
-  clientPromise = globalThis.mongoClientPromise;
-} else {
-  // En producción, siempre creamos una nueva conexión
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+if (!uri) {
+  throw new Error("Falta MONGODB_URI en .env.local");
 }
+
+const client = new MongoClient(uri, options);
+const clientPromise: Promise<MongoClient> = client.connect();
 
 export default clientPromise;
